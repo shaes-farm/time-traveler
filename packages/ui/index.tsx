71e953d9c0a2f4b7800d227dc4fbb52abe8b7fa0
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-12 20:39:44 -0400

    Add CardButtons component

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,4 +1,5 @@
 // component exports
 export { Breadcrumbs } from './src/breadcrumbs';
+export { CardButtons } from './src/card-buttons';
 export { HorizontalStepper } from './src/horizontal-stepper';
 export { VerticalTimeline } from './src/vertical-timeline';
---
+++ packages/ui/src/card-buttons.tsx
@@ -0,0 +1,37 @@
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import SubscribeIcon from '@mui/icons-material/AlternateEmail';
+
+interface CardButtonsProps {
+  onLike: () => void;
+  onShare: () => void;
+  onSubscribe: () => void;
+}
+
+export function CardButtons(props: CardButtonsProps): JSX.Element {
+  const {onLike, onShare, onSubscribe} = props;
+  return (
+    <Box>
+      <Tooltip title="Like">
+        <IconButton aria-label="like" onClick={onLike}>
+          <FavoriteIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Share">
+        <IconButton aria-label="share" onClick={onShare}>
+          <ShareIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Subscribe">
+        <IconButton aria-label="subscribe" onClick={onSubscribe}>
+          <SubscribeIcon />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+}
+
+export default CardButtons;
