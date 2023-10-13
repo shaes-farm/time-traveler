12ac2bde10997d64665f15290ff1564f09138a89
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-13 06:25:58 -0400

    Move to components folder

---
+++ packages/ui/src/components/index.tsx
@@ -0,0 +1,9 @@
+// component exports
+export { Breadcrumbs } from './breadcrumbs';
+export { CardButtons } from './card-buttons';
+export { Footer } from './footer';
+export { Header } from './header';
+export { HorizontalStepper } from './horizontal-stepper';
+export { MenuButton } from './menu-button';
+export { VerticalTimeline } from './vertical-timeline';
+export { WorldMap } from './world-map';
