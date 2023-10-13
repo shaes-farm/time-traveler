ddeaed4a2fbcbdc84e7544c3bcd19f5574b010f7
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-13 06:26:14 -0400

    Update top-level exports

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,8 +1,1 @@
-// component exports
-export { Breadcrumbs } from './src/breadcrumbs';
-export { CardButtons } from './src/card-buttons';
-export { Footer } from './src/footer';
-export { Header } from './src/header';
-export { HorizontalStepper } from './src/horizontal-stepper';
-export { MenuButton } from './src/menu-button';
-export { VerticalTimeline } from './src/vertical-timeline';
+export * from './src/components';
