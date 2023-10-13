eef8be974e960462adb39d3984ee1179b160fac5
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-12 21:46:58 -0400

    Add header & footer to index

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,6 +1,8 @@
 // component exports
 export { Breadcrumbs } from './src/breadcrumbs';
 export { CardButtons } from './src/card-buttons';
-export { MenuButton } from './src/menu-button';
+export { Footer } from './src/footer';
+export { Header } from './src/header';
 export { HorizontalStepper } from './src/horizontal-stepper';
+export { MenuButton } from './src/menu-button';
 export { VerticalTimeline } from './src/vertical-timeline';
