cfdeccb1df01f4a52742fbfc3b5ee1b4b2c1f378
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-13 20:49:04 -0400

    Move config to file

---
+++ packages/ui/src/stories/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+  app: {
+    baseUrl: 'http://localhost:6006',
+    title: 'The Application Title',
+    description: 'The application sub-title / description',
+    copyright: {
+      year: 2023,
+      url: 'http://example.com',
+      holder: 'Jimbo Pickins',
+    }
+  }
+};
