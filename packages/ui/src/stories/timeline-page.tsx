987c3580d7d87df816588f6ba530201ca2a0e349
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-14 13:56:32 -0400

    Add menus to timeline page stories

--- packages/ui/src/stories/timeline-page.tsx
+++ packages/ui/src/stories/timeline-page.tsx
@@ -6,10 +6,32 @@ import {TimelineCard} from '../cards/timeline-card';
 import {config} from './config';
 import {timeline} from './timeline';
 
+const headerMenu = [{
+  label: 'home',
+  route: '/',
+},{
+  label: 'about',
+  route: '/about',
+},{
+  label: 'contact',
+  route: '/contact',
+}];
+
+const footerMenu = [{
+  label: 'disclaimer',
+  route: '/disclaimer',
+},{
+  label: 'privacy policy',
+  route: '/privacy',
+},{
+  label: 'terms of use',
+  route: '/terms',
+}];
+
 export function Page(): JSX.Element {
   return (
     <Container maxWidth="lg" sx={{ m: 'auto' }}>
-      <Header app={config.app}/>
+      <Header app={config.app} menu={headerMenu} />
       <Breadcrumbs crumbs={[
         {
           link: config.app.baseUrl,
@@ -26,7 +48,7 @@ export function Page(): JSX.Element {
       <main>
         <TimelineCard timeline={timeline} />
       </main>
-      <Footer app={config.app}/>
+      <Footer app={config.app} menu={footerMenu} />
     </Container>
   );
 };
