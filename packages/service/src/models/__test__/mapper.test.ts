13ce6e2147cbee614a1615f0bccad965e8fa9d3b
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-15 15:46:41 -0400

    Update mapper unit test

--- packages/service/src/models/__test__/mapper.test.ts
+++ packages/service/src/models/__test__/mapper.test.ts
@@ -1,20 +1,17 @@
 import {faker} from '@faker-js/faker';
+import type {StrapiCategory} from '../api-model';
 import * as Mapper from '../mapper';
 
-const STRAPI_CATEGORY = {
-  data: [{
-    attributes: {
-      slug: faker.lorem.slug(),
-      title: faker.lorem.words({min: 1, max: 8}),
-    }
-  }]
+const STRAPI_CATEGORY: StrapiCategory = {
+  slug: faker.lorem.slug(),
+  title: faker.lorem.words({min: 1, max: 8}),
 };
 
 describe('model mappers', () => {
-  it('should map from one thing to another', () => {
+  it('should map from one category to another', () => {
     expect(Mapper.mapApiCategoryToModel(STRAPI_CATEGORY)).toEqual({
-      slug: expect.any(String),
-      title: expect.any(String),
+      slug: STRAPI_CATEGORY.slug,
+      title: STRAPI_CATEGORY.title,
     })
   });
 });
