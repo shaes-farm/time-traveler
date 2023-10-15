2ffa6321e02820831182999fcb58ff58fc932a1d
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-15 13:51:24 -0400

    Refactor web app main page to use PeriodNavigator

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,7 @@
 import config from 'config';
 // import Image from 'next/image'
 import {Fetch} from 'service';
-import {
-  HorizontalStepper,
-  VerticalTimeline,
-} from 'ui';
+import {PeriodNavigator} from 'ui';
 import styles from './page.module.css'
 
 const {debug} = console;
@@ -13,19 +10,11 @@ const f = new Fetch(config.get('api.baseUrl'));
 
 export default async function Home(): Promise<JSX.Element> {
   const periods = await f.getPeriods();
-  const timelines = await f.getTimelines();
-
   debug({periods});
 
   return (
     <main className={styles.main}>
-      <HorizontalStepper steps={
-        periods.map((period) => ({
-          label: period.title,
-          link: `/periods/${period.slug}`,
-        }))
-      } />
-      <VerticalTimeline timeline={timelines[0]} />
+      <PeriodNavigator periods={periods} />
     </main>
   )
 }
