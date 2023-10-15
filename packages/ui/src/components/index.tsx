0aa3e37a20363a2b431bd1cdf61acc21a1f36fad
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-15 13:38:15 -0400

    Add menu to ui components export

--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -4,6 +4,7 @@ export { CardButtons } from './card-buttons';
 export { Footer } from './footer';
 export { Header } from './header';
 export { HorizontalStepper } from './horizontal-stepper';
+export { Menu } from './menu';
 export { MenuButton } from './menu-button';
 export { VerticalTimeline } from './vertical-timeline';
 export { WorldMap } from './world-map';
