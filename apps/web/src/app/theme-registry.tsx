0b8ded9bce9a779bb4995ed86e538166e9f01909
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-17 06:53:50 -0400

    Add brackets to theme import

--- apps/web/src/app/theme-registry.tsx
+++ apps/web/src/app/theme-registry.tsx
@@ -5,8 +5,7 @@ import { useServerInsertedHTML } from 'next/navigation';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-
-import theme from './theme';
+import {theme} from './theme';
 
 interface ThemeRegistryProps {
   options: unknown;
