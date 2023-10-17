b3e060565dc1b3e5b5dd8c3b1cdf12e65ac98d2a
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-17 06:53:35 -0400

    Add Container, simplify logic

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,20 +1,18 @@
 import config from 'config';
-// import Image from 'next/image'
-import {Fetch} from 'service';
+import {Container} from '@mui/material';
 import {PeriodNavigator} from 'ui';
-import styles from './page.module.css'
-
-const {debug} = console;
+import {Fetch} from 'service';
 
 const f = new Fetch(config.get('api.baseUrl'));
 
 export default async function Home(): Promise<JSX.Element> {
   const periods = await f.getPeriods();
-  debug({periods});
 
   return (
-    <main className={styles.main}>
-      <PeriodNavigator periods={periods} />
+    <main>
+      <Container>
+        <PeriodNavigator periods={periods} />
+      </Container>
     </main>
   )
 }
