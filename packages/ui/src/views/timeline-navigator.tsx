f85916654a3722af870de08405bcbc559af8fc76
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-24 06:23:36 -0400

    Remove Container wrapper

--- packages/ui/src/views/timeline-navigator.tsx
+++ packages/ui/src/views/timeline-navigator.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {useState} from 'react';
-import {Box, Container, Paper, Stack, Typography} from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import {Box, Paper, Stack, Typography} from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2'; // MUI Grid v2
 import type {Timeline, HistoricalEvent} from 'service';
 import {EventTimeline} from './event-timeline';
 
@@ -15,70 +15,68 @@ export function TimelineNavigator(props: TimelineNavigatorProps): JSX.Element {
   const [event, setEvent] = useState<HistoricalEvent | null>(timeline.events[0] ?? null);
 
   return (
-    <Container>
-      <Stack spacing={2}>
-        <Paper elevation={3}>
-          <Box sx={{ m: 2, py: 1, textAlign: 'center' }}>
-            <Typography variant="h2">
-              {title}
-            </Typography>
-            <Typography gutterBottom>
-              {summary}
-            </Typography>
-          </Box>
-        </Paper>
-        <Box sx={{ textAlign: 'center' }}>
-          <Grid container spacing={2}>
-            <Grid md={5}>
-              <Paper elevation={3} >
-                <Typography component="h3" variant="h5">
-                  Time span: {beginDate}-{endDate}
-                </Typography>
-                {scale ? <Typography component="sub" variant="caption">
-                  ({scale})
-                </Typography> : null}
-                <EventTimeline
-                  alternate={false}
-                  events={timeline.events}
-                  onSelect={(slug: string) => {
-                    setEvent(timeline.events.find((e) => e.slug === slug) ?? null);
-                  }}
-                  opposite
-                  outlined
-                  reverse
-                  summary={false}
-                />
-              </Paper>
-            </Grid>
-            <Grid md={7}>
-              <Paper elevation={3}>
-                {event ? <>
-                  <Typography variant="h3">
-                    {event.title}
-                  </Typography>
-                  {event.location ? <Typography color="text.secondary" component="em" display="block">
-                    {event.location}
-                  </Typography> : null}
-                  <Typography color="text.secondary">
-                    {event.beginDate}{event.endDate ? <>`-${event.endDate}`</> : null}
-                  </Typography>
-                  {event.summary ? <Typography py={2}>
-                    {event.summary}
-                  </Typography> : null}
-                  {event.media.length ? <Typography>
-                    {event.media.map((item, index) => (
-                      <div key={Symbol(index).toString()}>{item.caption}</div>
-                    ))}
-                  </Typography> : null}
-                  {event.detail ? <Typography py={2}>
-                    {event.detail}
-                  </Typography> : null}
-                </> : null}
-              </Paper>
-            </Grid>
-          </Grid>
+    <Stack spacing={2}>
+      <Paper elevation={3}>
+        <Box sx={{ m: 2, py: 1, textAlign: 'center' }}>
+          <Typography variant="h2">
+            {title}
+          </Typography>
+          <Typography gutterBottom>
+            {summary}
+          </Typography>
         </Box>
-      </Stack>
-    </Container>
+      </Paper>
+      <Box sx={{ textAlign: 'center' }}>
+        <Grid container spacing={2}>
+          <Grid md={5} sm={12} xs={12}>
+            <Paper elevation={3}>
+              <Typography component="h3" variant="h5">
+                Spans {beginDate} to {endDate}
+              </Typography>
+              {scale ? <Typography component="sub" variant="caption">
+                ({scale})
+              </Typography> : null}
+              <EventTimeline
+                alternate={false}
+                events={timeline.events}
+                onSelect={(slug: string) => {
+                  setEvent(timeline.events.find((e) => e.slug === slug) ?? null);
+                }}
+                opposite
+                outlined
+                reverse
+                summary={false}
+              />
+            </Paper>
+          </Grid>
+          <Grid md={7} sm={12} xs={12}>
+            <Paper elevation={3}>
+              {event ? <>
+                <Typography variant="h3">
+                  {event.title}
+                </Typography>
+                {event.location ? <Typography color="text.secondary" component="em" display="block">
+                  {event.location}
+                </Typography> : null}
+                <Typography color="text.secondary">
+                  {event.beginDate}{event.endDate ? <>-{event.endDate}</> : null}
+                </Typography>
+                {event.summary ? <Typography py={2}>
+                  {event.summary}
+                </Typography> : null}
+                {event.media.length ? <Typography>
+                  {event.media.map((item, index) => (
+                    <div key={Symbol(index).toString()}>{item.caption}</div>
+                  ))}
+                </Typography> : null}
+                {event.detail ? <Typography color="text.secondary" py={2}>
+                  {event.detail}
+                </Typography> : null}
+              </> : null}
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
+    </Stack>
   );
 };
