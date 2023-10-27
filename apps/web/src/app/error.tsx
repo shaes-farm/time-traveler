607ca45b0701b42884be188456db3f6c243095c5
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-10-27 06:24:37 -0400

    Add dump of error to error boundary

--- apps/web/src/app/error.tsx
+++ apps/web/src/app/error.tsx
@@ -5,17 +5,18 @@ import {CustomError} from 'ui';
  
 export default function Error({
   error,
-  reset,
+  _reset,
 }: {
   error: Error & { status?: number; digest?: string; }
-  reset: () => void
+  _reset: () => void
 }): JSX.Element {
   useEffect(() => {
     // Log the error to an error reporting service
+    // eslint-disable-next-line no-console -- console for now
     console.error(error)
   }, [error])
  
   return (
-    <CustomError message={error.message} status={error.status} />
+    <CustomError dump={error} message={error.message} status={error.status} />
   )
 }
