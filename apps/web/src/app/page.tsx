2099a35f5664a4a06a75af3a6b77cfbb7917bfd6
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-12-22 06:33:11 -0500

    Add new fetch, config, and factory

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,15 @@
-import config from 'config';
+import getConfig from 'next/config';
 import {Paper} from '@mui/material';
 import {PeriodNavigator} from 'ui';
-import {Fetch} from 'service';
+import {FetchFactory} from 'service';
+import type {ServerRuntimeConfig} from '../types';
 
-const f = new Fetch(config.get('api.baseUrl'));
+const {serverRuntimeConfig} = getConfig();
+
+const {api} = serverRuntimeConfig as ServerRuntimeConfig;
 
 export default async function Page(): Promise<JSX.Element> {
+  const f = FetchFactory.create(api.backend, api.baseUrl);
   const periods = await f.getPeriods();
   return (
     <main>
--- apps/web/src/app/timeline/[slug]/page.tsx
+++ apps/web/src/app/timeline/[slug]/page.tsx
@@ -1,12 +1,13 @@
-import config from 'config';
+import getConfig from 'next/config';
 import {notFound} from 'next/navigation'
-import {Breadcrumbs, TimelineNavigator} from 'ui';
-import {Fetch} from 'service';
-import type {AppConfig} from '../../../types';
+import {Breadcrumbs, type Crumb, TimelineNavigator} from 'ui';
+import {FetchFactory} from 'service';
+import type {PublicRuntimeConfig, ServerRuntimeConfig} from '../../../types';
 
-const app: AppConfig = config.get('app');
+const {publicRuntimeConfig, serverRuntimeConfig} = getConfig();
 
-const f = new Fetch(config.get('api.baseUrl'));
+const {app} = publicRuntimeConfig as PublicRuntimeConfig;
+const {api} = serverRuntimeConfig as ServerRuntimeConfig;
 
 interface PageProps {
   params: {
@@ -16,21 +17,26 @@ interface PageProps {
 
 export default async function Page(props: PageProps): Promise<JSX.Element> {
   const {params} = props;
+  const f = FetchFactory.create(api.backend, api.baseUrl);
   const timeline = await f.getTimeline(params.slug);
 
   if (!timeline) {
     notFound();
   }
 
-  const crumbs = [{
+  const crumbs: Crumb[] = [{
     label: app.title,
     link: '/',
-  },{
+  }];
+  
+  crumbs.push({
     label: 'Period One',
     link: '/periods/[slug]',
-  },{
+  });
+  
+  crumbs.push({
     label: timeline.title,
-  }];
+  });
 
   return (
     <>
