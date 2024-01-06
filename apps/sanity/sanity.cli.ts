edd036295d3079dddec5800febcb22c0a904f283
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-01-06 18:38:24 -0500

    Remove Sanity.io from the project

--- apps/sanity/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'l4b7bear',
-    dataset: 'production'
-  }
-})
--- packages/service/src/models/index.ts
+++ packages/service/src/models/index.ts
@@ -1,5 +1,4 @@
 export * from './postgrest-model';
-export * from './sanity-model';
 export * from './strapi-model';
 export * from './supabase-model';
 export * from './app-model';
