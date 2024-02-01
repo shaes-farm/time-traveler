530e846ee55641604d864aae401d6e5b13423eaf
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-01-31 20:03:43 -0500

    Add back button to content editor

--- apps/admin/src/components/content-editor.tsx
+++ apps/admin/src/components/content-editor.tsx
@@ -1,7 +1,10 @@
 'use client';
 import React from 'react';
+import {useRouter} from 'next/navigation';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {
   Box,
+  Button,
   Typography,
   Unstable_Grid2 as Grid2
 } from '@mui/material';
@@ -12,10 +15,14 @@ interface ContentEditorProps {
 }
 
 export function ContentEditor({ title, children }: ContentEditorProps): JSX.Element {
+  const router = useRouter();
   return (
     <Box sx={{ p: '1em', mt: '3em', width: '100%' }}>
       <Grid2 container mb="1em">
         <Grid2 xs={12}>
+          <Button aria-label="back" color="secondary" onClick={() => {router.back()}}>
+            <ArrowBackIcon />&nbsp;Back
+          </Button>
           <Typography color="text.primary" variant="h2">
             {title}
           </Typography>
