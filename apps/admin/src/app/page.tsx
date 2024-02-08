328dffc8e7b76c6bbfd168a2b74af9bc45b590bf
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-02-08 06:23:42 -0500

    Refactor main page to validate authentication

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,26 +1,34 @@
 import getConfig from 'next/config';
-import {fetchFactory} from 'service';
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import type {NextConfig} from '../types';
+import { createClient } from '../utils/supabase/server'
 
 const {
   serverRuntimeConfig: {
     api: {
       baseUrl,
-      backend,
+      key,
     }
   }
 } = getConfig() as NextConfig;
 
-const f = fetchFactory(backend, baseUrl);
+export default async function Page(): Promise<JSX.Element | null> {
+  const cookieStore = cookies()
+  const supabase = createClient(baseUrl, key, cookieStore)
 
-export default async function Page(): Promise<JSX.Element> {
-  const timelines = await f.getTimelines();
+  const { data, error } = await supabase.auth.getUser()
 
-  return (
-    <main>
-      <pre>
-        {JSON.stringify(timelines, null, 2)}
-      </pre>
-    </main>
-  )
+  const {log} = console;
+
+  log({data});
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- test data
+  if (error || !data?.user) {
+    redirect('/signin')
+  }
+
+  redirect('/dashboard');
+ 
+  return null;
 }
