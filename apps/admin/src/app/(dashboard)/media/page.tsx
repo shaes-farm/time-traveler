b4d8ca7de3452a0250cd6c96b50bc12ae576da34
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-02-18 12:59:09 -0500

    Refactor media page to tabbed view

--- apps/admin/src/app/(dashboard)/media/page.tsx
+++ apps/admin/src/app/(dashboard)/media/page.tsx
@@ -1,7 +1,7 @@
 import getConfig from 'next/config';
-import {fetchFactory} from 'service';
-import type {NextConfig} from '../../../types';
-import {MediaListView} from '../../../views';
+import { fetchFactory } from 'service';
+import type { NextConfig } from '../../../types';
+import { MediaTabView } from '../../../views';
 
 const {
   serverRuntimeConfig: {
@@ -16,12 +16,8 @@ const f = fetchFactory(backend, baseUrl);
 
 export default async function Page(): Promise<JSX.Element> {
   const media = await f.getMedia();
+
   return (
-    <MediaListView
-      createLink="/media/create"
-      deleteLink="/media/[slug]/delete"
-      editLink="/media/[slug]"
-      media={media}
-    />
+    <MediaTabView media={media} />
   );
 }
