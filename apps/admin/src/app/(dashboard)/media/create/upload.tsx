40a27ee11dbe75d95e31c9907f61e18735369f71
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-02-18 13:03:40 -0500

    Add height / width to upload component

--- apps/admin/src/app/(dashboard)/media/create/upload.tsx
+++ apps/admin/src/app/(dashboard)/media/create/upload.tsx
@@ -8,27 +8,33 @@ import { addMedia, upload } from '../../actions';
 
 const debug = debugFactory('admin:app:media:upload');
 
-function onError(error: Error): void {
-    debug(`Failed because of ${error.message}`);
-}
-
-function onSuccess(info: UploadInfo): void {
-    const media: Media = {
-        alternativeText: info.fileName,
-        formats: info.fileType,
-        slug: slugify(info.fileName, { lower: true, strict: true }),
-        url: `/storage/v1/object/public/media/${encodeURIComponent(info.fileName)}`,
-        userId: info.userId,
-    };
-    debug({ media });
-    addMedia(media).catch((error: unknown) => {
-        onError(error as Error);
-    });
-};
-
 export function Upload(): JSX.Element {
+    function onSuccess(info: UploadInfo): void {
+        const img = new Image();
+        img.src = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/media/${encodeURIComponent(info.fileName)}?quality=100`;
+        debug('upload', {img});
+        setTimeout(() => {
+            debug('height', img.naturalHeight);
+            debug('width', img.naturalWidth);
+            const media: Media = {
+                alternativeText: info.fileName,
+                formats: info.fileType,
+                height: img.naturalHeight ? img.naturalHeight : undefined,
+                slug: slugify(info.fileName, { lower: true, strict: true }),
+                url: encodeURIComponent(info.fileName),
+                userId: info.userId,
+                width: img.naturalWidth ? img.naturalWidth : undefined,
+            };
+            debug({ media });
+            addMedia(media).catch((error: unknown) => {
+                const { message } = error as Error;
+                debug(`Failed because of ${message}`);
+            });
+        }, 3);
+    };
+
     return (
-        <DragAndDropUpload upload={(file, setProgress) => {
+        <DragAndDropUpload upload={(file, setProgress, onError) => {
             void upload('media', file, setProgress, onSuccess, onError);
         }} />
     );
