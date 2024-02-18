8480e1ba754664fbe96a5378f8638ba4a0a09f95
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-02-18 13:03:16 -0500

    Refactor media actions

--- apps/admin/src/app/(dashboard)/actions.ts
+++ apps/admin/src/app/(dashboard)/actions.ts
@@ -10,11 +10,16 @@ export async function upload(bucketName: string, fileUpload: FileUpload, setProg
 
   const supa = new SupabaseUpload(supabase);
 
+  const onReset = (info: UploadInfo): void => {
+    fileUpload.progress = 100;
+    onSuccess(info);
+  };
+
   await supa.resumableUpload(
     bucketName,
     fileUpload.file,
     setProgress,
-    onSuccess,
+    onReset,
     onError,
   );
 }
@@ -29,32 +34,34 @@ export async function addMedia(media: Media): Promise<void> {
     .select()
     .eq('slug', media.slug);
 
-  debug({lookupError, items});
+  debug('addMedia', {lookupError, items});
 
   if (!lookupError && items.length > 0) {
+    const db = {
+      user_id: media.userId,
+      slug: media.slug,
+      url: media.url,
+      formats: media.formats,
+    };
+    debug('addMedia->update', {db});
     await supabase
       .from('media')
-      .update({
-        user_id: media.userId,
-        slug: media.slug,
-        url: media.url,
-        formats: media.formats,
-      })
+      .update(db)
       .eq('slug', media.slug);
-    debug('update');
   } else {
+    const db = {
+      user_id: media.userId,
+      slug: media.slug,
+      alternativetext: media.alternativeText,
+      caption: media.caption,
+      url: media.url,
+      width: media.width,
+      height: media.height,
+      formats: media.formats,
+    };
+    debug('addMedia->insert', {db});
     await supabase
       .from('media')
-      .insert({
-        user_id: media.userId,
-        slug: media.slug,
-        alternativetext: media.alternativeText,
-        caption: media.caption,
-        url: media.url,
-        width: media.width,
-        height: media.height,
-        formats: media.formats,
-      });
-    debug('insert');
+      .insert(db);
   }
 }
