e88378dec6e2da2ea4f46734fdc805b6d05a2bfa
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-02-21 20:51:42 -0500

    Add period delete view

---
+++ apps/admin/src/app/(dashboard)/periods/[slug]/delete/period-delete-view.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import debugFactory from 'debug';
+import {useRouter} from 'next/navigation';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import type { Period } from 'service';
+import { ContentEditor } from '../../../../../components';
+import { remove } from '../../actions';
+
+const debug = debugFactory('admin:periods:delete:view');
+
+interface PeriodDeleteViewProps {
+    period: Period;
+}
+
+export function PeriodDeleteView({period}: PeriodDeleteViewProps): JSX.Element {
+  const router = useRouter();
+
+  debug({period});
+
+  return (
+    <ContentEditor title="Delete a Period">
+        <Typography sx={{fontSize: '2em'}} variant="body1">Are you sure you want to delete this period?</Typography>
+        <Typography variant="body1">{period.title}</Typography>
+        <Button color="secondary" onClick={() => {router.push('/periods')}}>
+          Cancel
+        </Button>
+        <Button color="primary" onClick={() => {void remove(period.slug)}}>
+          Confirm
+        </Button>
+    </ContentEditor>
+  );
+}
