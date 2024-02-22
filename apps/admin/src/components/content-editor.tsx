c3a2b86a4763be66c9cd465f9a869790da5c5d36
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-02-21 20:51:54 -0500

    Remove back button

--- apps/admin/src/components/content-editor.tsx
+++ apps/admin/src/components/content-editor.tsx
@@ -1,10 +1,8 @@
 'use client';
 import React from 'react';
 import {useRouter} from 'next/navigation';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {
   Box,
-  Button,
   Typography,
   Unstable_Grid2 as Grid2
 } from '@mui/material';
@@ -21,9 +19,6 @@ export function ContentEditor({ title, backLink, children }: ContentEditorProps)
     <Box sx={{ p: '1em', mt: '3em', width: '100%' }}>
       <Grid2 container mb="1em">
         <Grid2 xs={12}>
-          <Button aria-label="back" color="secondary" onClick={() => {backLink ? router.push(backLink) : router.back()}}>
-            <ArrowBackIcon />&nbsp;Back
-          </Button>
           <Typography color="text.primary" variant="h2">
             {title}
           </Typography>
--- apps/admin/src/components/content-viewer.tsx
+++ apps/admin/src/components/content-viewer.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import {useRouter} from 'next/navigation';
 import AddIcon from '@mui/icons-material/Add';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {
   Box,
   Button,
@@ -23,9 +22,6 @@ export function ContentViewer({title, count, createLink, children}: ContentViewe
     <Box sx={{ p: '1em', mt: '3em', width: '100%' }}>
       <Grid2 container>
         <Grid2 md={6} sm={12}>
-          <Button aria-label="back" color="secondary" onClick={() => {router.back()}}>
-            <ArrowBackIcon />&nbsp;Back
-          </Button>
           <Typography color="text.primary" variant="h2">
             {title}
           </Typography>
