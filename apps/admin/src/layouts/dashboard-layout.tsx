c3d0c4b28e094afa678e6c0c1160c5fed08ac70b
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-02-24 12:30:06 -0500

    Add Stack for vertical justification

--- apps/admin/src/layouts/dashboard-layout.tsx
+++ apps/admin/src/layouts/dashboard-layout.tsx
@@ -1,17 +1,17 @@
 'use client';
 import debugFactory from 'debug';
-import {useEffect, useState} from 'react';
-import {useRouter} from 'next/navigation';
-import {Container, Paper} from '@mui/material';
-import type {Profile} from 'service';
-import type {NavRoute, NavRouter} from 'ui';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Container, Paper, Stack } from '@mui/material';
+import type { Profile } from 'service';
+import type { NavRoute, NavRouter } from 'ui';
 import {
   Dashboard,
   Copyright,
   ProfileProvider,
 } from 'ui';
-import {MAIN_ROUTES, TOOLBAR_ROUTES} from '../app/constants';
-import {createClient} from '../utils/supabase/client';
+import { MAIN_ROUTES, TOOLBAR_ROUTES } from '../app/constants';
+import { createClient } from '../utils/supabase/client';
 
 const debug = debugFactory('admin:layouts:dashboard-layout');
 
@@ -23,7 +23,7 @@ interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
-export function DashboardLayout({name, url, year, userProfile, children}: DashboardLayoutProps): JSX.Element {
+export function DashboardLayout({ name, url, year, userProfile, children }: DashboardLayoutProps): JSX.Element {
   const [profile, setProfile] = useState<Profile>(userProfile);
   const nextRouter = useRouter();
   const supabase = createClient();
@@ -32,12 +32,12 @@ export function DashboardLayout({name, url, year, userProfile, children}: Dashbo
     supabase.auth.onAuthStateChange((event, session) => {
       if (event === 'SIGNED_OUT') {
         debug(event, session)
-  
+
         const storageList = [
           window.localStorage,
           window.sessionStorage,
         ];
-  
+
         // clear local and session storage
         storageList.forEach((storage) => {
           Object.entries(storage)
@@ -46,12 +46,16 @@ export function DashboardLayout({name, url, year, userProfile, children}: Dashbo
             })
         });
 
-        setTimeout(() => {nextRouter.push('/signin')});
+        setTimeout(() => { nextRouter.push('/signin') });
       } else if (event === 'PASSWORD_RECOVERY') {
         debug(event, session);
       } else if (event === 'TOKEN_REFRESHED') {
         debug(event, session);
       } else if (event === 'USER_UPDATED') {
+        debug(event, session);
+      } else if (event === 'INITIAL_SESSION') {
+        debug(event, session);
+      } else {
         debug(event, session);
       }
     });
@@ -69,21 +73,27 @@ export function DashboardLayout({name, url, year, userProfile, children}: Dashbo
   return (
     <ProfileProvider profile={profile} setProfile={setProfile} >
       <Dashboard
-          router={router}
-          routes={MAIN_ROUTES}
-          toolbar={TOOLBAR_ROUTES}
+        router={router}
+        routes={MAIN_ROUTES}
+        toolbar={TOOLBAR_ROUTES}
       >
-          <Container maxWidth="lg" sx={{ m: 'auto' }}>
-          <Paper>
+        <Container maxWidth="lg" sx={{ m: 'auto' }}>
+          <Stack
+            alignItems="stretch"
+            justifyContent="flex-end"
+            useFlexGap
+          >
+            <Paper>
               {children}
-          </Paper>
-          <Copyright
+            </Paper>
+            <Copyright
               holder={name}
-              sx={{ pt: 4 }}
+              sx={{ marginTop: 'auto', pt: 4 }}
               url={url}
               year={year}
-          />
-          </Container>
+            />
+          </Stack>
+        </Container>
       </Dashboard>
     </ProfileProvider>
   );
