9b1610b0e1796cfcf336c68f11d6474d678dd274
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-03-10 15:45:23 -0400

    Add icon button components

---
+++ packages/ui/src/components/link-icon-button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+
+export interface LinkIconButtonProps {
+  /**
+   * The icon to link.
+   */
+  icon: React.ReactNode;
+  /**
+   * Route to navigate to when icons is clicked.
+   */
+  resource: URL | string;
+  /**
+   * The link target.
+   */
+  target?: string;
+  /**
+   * The button tooltip.
+   */
+  tooltip?: string;
+  /**
+   * The link features
+   */
+  features?: string;
+}
+
+/**
+ * Messages component that combines a Mail icon with a tooltip and a badge showing a count of messages. Calls the router with the route when clicked.
+ */
+export function LinkIconButton({ icon, resource, target, tooltip, features }: LinkIconButtonProps): JSX.Element {
+  return (
+    <Tooltip title={tooltip}>
+      <IconButton onClick={() => { window.open(resource, target, features) }}>
+        {icon}
+      </IconButton>
+    </Tooltip>
+  );
+}
---
+++ packages/ui/src/components/social-icon-button.tsx
@@ -0,0 +1,44 @@
+import XIcon from '@mui/icons-material/X';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import PublicIcon from '@mui/icons-material/Public';
+import {
+  IconButton,
+  Tooltip,
+} from '@mui/material';
+
+const ICONS = {
+  x: <XIcon />,
+  twitter: <XIcon />,
+  facebook: <FacebookIcon />,
+  instagram: <InstagramIcon />,
+  pinterest: <PinterestIcon />,
+  youtube: <YouTubeIcon />,
+  website: <PublicIcon />,
+};
+
+export interface SocialIconButtonProps {
+  /**
+   * The type of link.
+   */
+  type: 'x' | 'twitter' | 'facebook' | 'instagram' | 'pinterest' | 'youtube' | 'website';
+  /**
+   * Social account name, or site to navigate to when icon is clicked.
+   */
+  resource: string;
+}
+
+/**
+ * Social media icon that opens in a new window / tab.
+ */
+export function SocialIconButton({ resource, type }: SocialIconButtonProps): JSX.Element {
+  return (
+    <Tooltip title={resource}>
+      <IconButton onClick={() => { window.open(resource, "_blank", "noopener,noreferrer") }}>
+        {ICONS[type]}
+      </IconButton>
+    </Tooltip>
+  );
+}
