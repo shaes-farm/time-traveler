322bbf0761b3fc06370ff1865bfac2fcf9a36444
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-03-23 10:46:38 -0400

    Add item list component

--- apps/admin/src/components/index.tsx
+++ apps/admin/src/components/index.tsx
@@ -2,4 +2,5 @@ export * from './content-viewer';
 export * from './content-editor';
 export * from './editor';
 export * from './grid-list';
+export * from './item-list';
 export * from './transfer-list';
