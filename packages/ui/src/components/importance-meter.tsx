7491b92fffde438f0b508441c98891acafbc38e3
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-03-30 11:13:33 -0400

    Add descriptions to importance meter

--- packages/ui/src/components/importance-meter.tsx
+++ packages/ui/src/components/importance-meter.tsx
@@ -4,9 +4,26 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Divider from '@mui/material/Divider';
-// import Grid from '@mui/material/Unstable_Grid2';
+import Grid from '@mui/material/Unstable_Grid2';
 import Slider from '@mui/material/Slider';
 import Typography from '@mui/material/Typography';
+
+const IMPORTANCE_DESCRIPTIONS = [
+    '1 - Least important',
+    '2 - Less important',
+    '3 - Less important',
+    '4 - Less important',
+    '5 - Moderatly important',
+    '6 - Moderatly important',
+    '7 - More important',
+    '8 - More important',
+    '9 - More important',
+    '10 - Most important',
+];
+
+function getValueText(value: number): string {
+    return value >= 1 && value <= 10 ? IMPORTANCE_DESCRIPTIONS[value - 1] : `${value}`;
+}
 
 interface ImportanceMeterProps {
     importance: number;
@@ -32,21 +49,32 @@ export function ImportanceMeter({ importance, onChange }: ImportanceMeterProps):
             />
             <Divider />
             <CardContent>
-                {/* <Grid container spacing={2}>
-                    <Grid mb={2} sm={6} xs={12}> */}
-                <Slider
-                    aria-labelledby="importance-slider"
-                    getAriaValueText={(value: number) => value.toString()}
-                    marks
-                    max={10}
-                    min={1}
-                    onChange={handleChange}
-                    step={1}
-                    value={importance}
-                    valueLabelDisplay="auto"
-                />
-                {/* </Grid>
-                </Grid> */}
+                <Grid container spacing={0}>
+                    <Grid xs={12}>
+                        <Slider
+                            aria-labelledby="importance-slider"
+                            getAriaValueText={getValueText}
+                            marks
+                            max={10}
+                            min={1}
+                            onChange={handleChange}
+                            step={1}
+                            value={importance}
+                            valueLabelDisplay="auto"
+                        />
+                    </Grid>
+                    <Grid textAlign="center" xs={12}>
+                        <Typography variant="caption">
+                            {/* select a value between 1 and 10 */}
+                            {getValueText(importance)}
+                        </Typography>
+                    </Grid>
+                    <Grid textAlign="center" xs={12}>
+                        <Typography variant="caption">
+                            1 is least important, 10 is most important
+                        </Typography>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     );
