fac6b10dea8bf5e655c05d927e84847e169fd579
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-04-06 21:07:51 -0400

    Render placeholder images

--- apps/admin/src/app/(dashboard)/media/image-view.tsx
+++ apps/admin/src/app/(dashboard)/media/image-view.tsx
@@ -2,7 +2,7 @@
 
 /* eslint-disable @next/next/no-img-element -- allow unoptimized img */
 import React from 'react';
-import Link from 'next/link';
+import Link from '@mui/material/Link';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
@@ -18,8 +18,21 @@ const WEB_IMAGE_FILE_TYPES = [
     'image/webp',
 ];
 
+const FILE_TYPE_ICONS = [{
+    type: 'application/pdf',
+    image: '/images/adobe-pdf-icon.svg'
+}];
+
 function isCommonImageType(type: string | undefined): boolean {
     return type ? WEB_IMAGE_FILE_TYPES.includes(type) : false;
+}
+
+function getIcon(type: string, fileName: string): string {
+    if (isCommonImageType(type)) {
+        return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/media/${fileName}?quality=100`;
+    }
+
+    return FILE_TYPE_ICONS.find((icon) => icon.type === type)?.image ?? '/images/placeholder.svg';
 }
 
 interface MediaImageViewProps {
@@ -37,8 +50,8 @@ export function MediaImageView({ media }: MediaImageViewProps): JSX.Element {
                     <img
                         alt={item.alternativeText}
                         loading="lazy"
-                        src={isCommonImageType(item.formats) ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/media/${item.url}?quality=100` : '/images/placeholder.svg'}
-                        srcSet={isCommonImageType(item.formats) ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/media/${item.url}?quality=100` : '/images/placeholder.svg'}
+                        src={getIcon(item.formats ?? '', item.url)}
+                        srcSet={getIcon(item.formats ?? '', item.url)}
                     />
                     <Link href={`/media/${item.slug}`}>
                         <ImageListItemBar
