ea5c5b246a19f23e12d4711f80282e1c7e624e51
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-04-06 21:20:29 -0400

    Refactor ItemTable component to support edit on click

--- apps/admin/src/components/item-table.tsx
+++ apps/admin/src/components/item-table.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment -- MUIDataTable is forked */
+/* eslint-disable import/no-named-as-default -- MUIDataTable typescript issue */
 'use client';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import MUIDataTable from "mui-datatables";
 import type { MUIDataTableColumnDef } from "mui-datatables";
@@ -27,18 +26,12 @@ export interface ItemTableColOptions {
   viewColumns?: boolean;
 }
 
-// export interface ItemTableCol {
-//   name: string;
-//   label: string;
-//   options?: ItemTableColOptions;
-// }
-
 export type ItemTableCol = MUIDataTableColumnDef;
 
 export interface ItemTableOptions {
   elevation?: number;
   filter?: boolean | 'disabled';
-  filterType?: 'checkbox' | 'dropdown' | 'mltiselect' | 'textField' | 'custom';
+  filterType?: 'checkbox' | 'dropdown' | 'multiselect' | 'textfield' | 'custom';
   search?: boolean | 'disabled';
   selectableRows?: 'multiple' | 'single';
   selectableRowsHeader?: boolean;
@@ -46,21 +39,15 @@ export interface ItemTableOptions {
   viewColumns?: boolean | 'disabled';
 }
 
-interface ActionsColumnProps {
-  onEdit: () => void;
+interface DeleteColumnProps {
   onDelete: () => void;
 }
 
-function ActionsColumn({onDelete, onEdit }: ActionsColumnProps): JSX.Element {
+function DeleteColumn({ onDelete/* , onEdit */ }: DeleteColumnProps): JSX.Element {
   return (
-    <Stack direction="row" spacing={0}>
-      <IconButton onClick={onEdit} size="small">
-        <EditIcon />
-      </IconButton>
-      <IconButton onClick={onDelete} size="small">
-        <DeleteIcon />
-      </IconButton>
-    </Stack>
+    <IconButton onClick={onDelete} size="small">
+      <DeleteIcon />
+    </IconButton>
   );
 }
 
@@ -73,28 +60,37 @@ interface ItemTableProps {
 }
 
 export function ItemTable({ title, columns, rows, deleteLink, editLink }: ItemTableProps): JSX.Element {
+  const [selectedRow, setSelectedRow] = useState<number|undefined>();
   const router = useRouter();
 
-  const handleEdit = (name: string) => () => {
+  useEffect(() => {
+    if (selectedRow !== undefined) {
+      handleEdit(rows[selectedRow].slug);
+      setSelectedRow(undefined);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps -- only render when selectedRow changes
+  }, [selectedRow]);
+
+  const handleEdit = (name: string): void => {
     const url = editLink.replace('[slug]', name.toString());
     router.push(url);
   };
 
-  const handleDelete = (name: string) => () => {
+  const handleDelete = (name: string): void => {
     const url = deleteLink.replace('[slug]', name.toString());
     router.push(url);
   };
 
   const columnDef = columns.concat([{
-    name: "Actions",
+    name: "Delete",
     options: {
       filter: false,
       sort: false,
       empty: true,
+      // eslint-disable-next-line react/no-unstable-nested-components -- allow component here
       customBodyRenderLite: (dataIndex, _rowIndex) => (
-        <ActionsColumn
-          onDelete={handleDelete(rows[dataIndex].slug)}
-          onEdit={handleEdit(rows[dataIndex].slug)}
+        <DeleteColumn
+          onDelete={(): void => {handleDelete(rows[dataIndex].slug)}}
         />
       ),
     }
@@ -113,6 +109,11 @@ export function ItemTable({ title, columns, rows, deleteLink, editLink }: ItemTa
           selectableRowsHeader: false,
           selectableRowsHideCheckboxes: true,
           selectableRowsOnClick: true,
+          onCellClick: (colData: unknown, cellMeta: { colIndex: number, rowIndex: number, dataIndex: number }): void => {
+            if (cellMeta.colIndex < columns.length) {
+              setSelectedRow(cellMeta.dataIndex);
+            }
+          },
         }}
         title={<Typography variant="h2">{title}</Typography>}
       />
