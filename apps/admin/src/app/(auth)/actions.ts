a239e9da4b9e016ee9a92bbfa5b5e98d97e345be
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-04-21 18:49:01 -0400

    Add reset to handle password reset

--- apps/admin/src/app/(auth)/actions.ts
+++ apps/admin/src/app/(auth)/actions.ts
@@ -1,20 +1,19 @@
 'use server'
-
-import getConfig from 'next/config';
+import debugLogger from 'debug';
 import { revalidatePath } from 'next/cache'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { createClient } from '../../utils/supabase/server'
-import type { NextConfig } from '../../types';
+import { logger } from '../../utils/logger';
+import { getAppConfig } from '../../utils/config';
+import { PASSWORD_RESET_URL, SIGNIN_URL } from './constants';
+
+const debug = debugLogger('admin:auth:actions');
 
 const {
-  publicRuntimeConfig: {
-    app: {
-      baseUrl: appBaseUrl,
-      basePath,
-    }
-  },
-} = getConfig() as NextConfig;
+  baseUrl: appBaseUrl,
+  basePath,
+} = getAppConfig();
 
 export async function login(formData: FormData): Promise<void> {
   const supabase = createClient(cookies());
@@ -29,11 +28,11 @@ export async function login(formData: FormData): Promise<void> {
   const { error } = await supabase.auth.signInWithPassword(data);
 
   if (error) {
+    logger.error({ error });
     throw error;
   }
 
   revalidatePath(`${appBaseUrl}${basePath}`, 'layout');
-
   redirect(`${appBaseUrl}${basePath}`);
 }
 
@@ -46,7 +45,7 @@ export async function signup(formData: FormData): Promise<void> {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
     options: {
-      emailRedirectTo: `${appBaseUrl}${basePath}/signin`,
+      emailRedirectTo: `${appBaseUrl}${basePath}${SIGNIN_URL}`,
       data: {
         first_name: formData.get('firstName') as string,
         last_name: formData.get('lastName') as string
@@ -57,31 +56,51 @@ export async function signup(formData: FormData): Promise<void> {
   const { error } = await supabase.auth.signUp(data);
 
   if (error) {
+    logger.error({ error });
     throw error;
   }
 
   revalidatePath(`${appBaseUrl}${basePath}`, 'layout');
-
   redirect(`${appBaseUrl}${basePath}`);
 }
 
 export async function recover(formData: FormData): Promise<void> {
-    const supabase = createClient(cookies());
-  
-    // type-casting here for convenience
-    // todo: validate inputs
-    const email = formData.get('email') as string;
-  
-    const { error } = await supabase.auth.resetPasswordForEmail(
-      email,
-      { redirectTo: `${appBaseUrl}${basePath}` }
-    );
-  
-    if (error) {
-      throw error;
-    }
-  
-    revalidatePath(`${appBaseUrl}${basePath}`, 'layout');
+  const supabase = createClient(cookies());
 
-    redirect(`${appBaseUrl}${basePath}`);
+  // type-casting here for convenience
+  // todo: validate inputs
+  const email = formData.get('email') as string;
+
+  const { error } = await supabase.auth.resetPasswordForEmail(
+    email,
+    { redirectTo: `${appBaseUrl}${basePath}${PASSWORD_RESET_URL}` }
+  );
+
+  if (error) {
+    logger.error({ error });
+    throw error;
   }
+
+  revalidatePath(`${appBaseUrl}${basePath}`, 'layout');
+  redirect(`${appBaseUrl}${basePath}`);
+}
+
+export async function reset(formData: FormData): Promise<void> {
+  const supabase = createClient(cookies());
+  
+  const password = formData.get('password') as string;
+
+  const { data, error } = await supabase.auth.updateUser({
+    password
+  });
+
+  debug('reset', { error, data });
+
+  if (error) {
+    logger.error({ error });
+    throw error;
+  }
+
+  revalidatePath(`${appBaseUrl}${basePath}`, 'layout');
+  redirect(`${appBaseUrl}${basePath}`);
+}
