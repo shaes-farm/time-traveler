3713773fdf30ef5a79578950dbc8e1659bd7ae0f
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2024-04-27 09:16:04 -0400

    Add navigation to the image

--- apps/admin/src/app/(dashboard)/media/image-view.tsx
+++ apps/admin/src/app/(dashboard)/media/image-view.tsx
@@ -2,40 +2,40 @@
 
 /* eslint-disable @next/next/no-img-element -- allow unoptimized img */
 import React from 'react';
-import Link from '@mui/material/Link';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import type { Media } from 'service';
-import {getIcon} from '../../../utils/media';
+import { Link } from '../../../components';
+import { getIcon } from '../../../utils/media';
 
 interface MediaImageViewProps {
-    media: Media[];
-    createLink: string;
-    deleteLink: string;
-    editLink: string;
+  media: Media[];
+  createLink: string;
+  deleteLink: string;
+  editLink: string;
 }
 
 export function MediaImageView({ media }: MediaImageViewProps): JSX.Element {
-    return (
-        <ImageList cols={4} sx={{ width: '100%', height: '500' }} variant="masonry">
-            {media.map((item) => (
-                <ImageListItem key={item.slug}>
-                    <img
-                        alt={item.alternativeText}
-                        loading="lazy"
-                        src={getIcon(item.formats ?? '', item.url)}
-                        srcSet={getIcon(item.formats ?? '', item.url)}
-                    />
-                    <Link href={`/media/${item.slug}`}>
-                        <ImageListItemBar
-                            position="below"
-                            subtitle={item.alternativeText}
-                            title={item.caption}
-                        />
-                    </Link>
-                </ImageListItem>
-            ))}
-        </ImageList>
-    );
+  return (
+    <ImageList cols={4} sx={{ width: '100%', height: '500' }} variant="masonry">
+      {media.map((item) => (
+        <Link href={`/media/${item.slug}`} key={item.slug}>
+          <ImageListItem>
+            <img
+              alt={item.alternativeText}
+              loading="lazy"
+              src={getIcon(item.formats ?? '', item.url)}
+              srcSet={getIcon(item.formats ?? '', item.url)}
+            />
+            <ImageListItemBar
+              position="below"
+              subtitle={item.alternativeText}
+              title={item.caption}
+            />
+          </ImageListItem>
+        </Link>
+      ))}
+    </ImageList>
+  );
 }
